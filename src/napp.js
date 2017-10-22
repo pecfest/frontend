6df5e71119f64fe3394b80e71f31cbc7a9e680fc
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2017-10-22 17:08:04 +0530

    Updated website, lectures and sponsors

--- src/napp.js
+++ src/napp.js
@@ -18,18 +18,40 @@ class Sponsors extends Component{
 
     }
 
-	render()
+
+		render()
 	{
 		window.j = 0;
 
 		function fin_ki_value(obj){return obj.id>=(window.j+1);}
 
 		window.data = this.props.data;
-		console.log(window.data);
 		const imglist = window.data.map((image,i) => {
-			console.log(image);
-			if(image.link===''){return (<img key={i} className="col-1" src={image.image} alt={image.name} title={image.name} />);}
-			else {return <div key={i}><a href={image.link} target="_blank"><img key={i} className="col-1" src={image.image} alt={image.name} title={image.name} /> </a></div>}
+			if(image.link===''){
+				return (
+				<div key={i} id="sponsors_f1_container" className="hover sponsors_col-1">
+					<div id="sponsors_f1_card" className="shadow sponsors_col-1">
+		  				<div className="front sponsors_face sponsors_col-1_image">
+	    					<img  className="sponsors_col-1" alt="" src={image.image} />
+	  						</div>
+	  					<div className="back sponsors_face center">
+				    <h3 className="sponsors_props_for_p">{image.name}	</h3>
+	  				</div>
+					</div>
+				</div>);}
+				else{
+					return (
+				<a href={image.link}><div key={i} id="sponsors_f1_container" className="hover sponsors_col-1">
+					<div id="sponsors_f1_card" className="shadow sponsors_col-1">
+		  				<div className="front sponsors_face sponsors_col-1_image">
+	    					<img  className="sponsors_col-1" alt="" src={image.image} />
+	  						</div>
+	  					<div className="back sponsors_face center">
+				    <h3 className="sponsors_props_for_p">{image.name}	</h3>
+	  				</div>
+					</div>
+				</div></a>);
+				}
 		});
 
 		window.type_of_spons = [];
@@ -40,23 +62,20 @@ class Sponsors extends Component{
 		{
 			final = window.data.findIndex(fin_ki_value);
 			if(final===-1){img_concat[window.j] = imglist.slice(init,imglist.length);window.type_of_spons[window.j] = window.data[init].type;break;}
-			console.log("final is ", final);
 			window.type_of_spons[window.j] = window.data[init].type;
 			img_concat[window.j] = imglist.slice(init,final);
 			init = final;
 		}
 
-		console.log(img_concat);
-		console.log(window.type_of_spons);
 		window.j = 0;
 
 		const final_arr = img_concat.map((imag,i) => {
 			return (
 					<div id="holder-div" key={i}>
-						<div className="App-indi-headers">
+						<div className="sponsors_App-indi-headers">
 						{window.type_of_spons[window.j++]}
 						</div>
-						<div className="flex-css" key={i}>
+						<div className="sponsors_flex-css" key={i}>
 							{imag.concat(" ")}
 						</div>
 					</div>
@@ -67,12 +86,13 @@ class Sponsors extends Component{
 
 
 		return (
-			<div id="main-div">
-			<div className="App-main-header" ><h1>Sponsors</h1></div>
+			<div id="sponsors_main-div">
+			<div className="sponsors_App-main-header" ><h1>Sponsors</h1></div>
 			{final_arr}
 			</div>
 		);
-	}
+
+   }
 }
 
 
--- src/sponsors.js
+++ src/sponsors.js
@@ -1,14 +1,19 @@
 import assoc1 from './images/colors_assoc.jpg';
 import assoc2 from './images/vh1_assoc.jpg';
-import assoc3 from './images/GNL.jpeg';
+import assoc3 from './images/bestech.jpg';
 import assoc4 from './images/comio.jpg';
-import assoc5 from './images/Coding_Ninjas.png';
+import assoc5 from './images/Bajaj Electricals.png';
 import assoc6 from './images/GBP.jpg';
 import assoc7 from './images/gsp.jpg';
 import assoc8 from './images/mercedes.png';
 import assoc9 from './images/Pokermet.png';
-import assoc10 from './images/Prachi.jpg';
+import assoc10 from './images/SBI.jpg';
 import assoc11 from './images/strands.jpg';
+import assoc12 from './images/imin.PNG';
+import assoc13 from './images/mtv.PNG';
+import assoc14 from './images/MTV Beats.jpg';
+import assoc15 from './images/Comedy Central.PNG';
+import assoc16 from './images/Droom_in.jpg';
 import offmedia1 from './images/tsn_official_media.jpg';
 import fmpartner1 from './images/fmpartner.jpg';
 import youthpartner1 from './images/pupulse_youth.jpg';
@@ -20,7 +25,20 @@ import cycling_partners from './images/hero.jpg';
 import diary_partner1 from './images/verka.jpg';
 import technology_partners1 from './images/zap.png';
 import overseas from './images/jmk.JPG';
-
+import merch1 from './images/montecarlo.jpg';
+import food1 from './images/coca_cola.jpg';
+import food2 from './images/cs_cafe.jpeg';
+import food3 from './images/eatstreet.jpeg';
+import food4 from './images/naman.jpeg';
+import food5 from './images/GNL.jpeg';
+import food6 from './images/Major Changs.png';
+import food7 from './images/Prachi.jpg';
+import food8 from './images/waiwai.jpg';
+import food9 from './images/taste battles.jpg';
+import safety_partner1 from './images/manforce.jpg';
+import coding_prtnr1 from './images/Coding_Ninjas.png';
+import mag_partner1 from './images/Chandigarh X.jpg';
+import tyre_partner1 from './images/Ralco.jpg';
 
 /*Keep the id of a particular type of sponsor to be the same throughout.*/
 
@@ -30,7 +48,7 @@ export default [
 		name : 'Honda',
 		image : t_sponsor1,
 		color : '',
-		type: 'Title Sponsors',
+		type: 'Title Sponsor',
 		link : ''
 	},
 	{
@@ -46,12 +64,47 @@ export default [
 		name : 'Talent edge',
 		image : co_spons_by,
 		color : '',
-		type: 'Co Sponsored By',
+		type: 'Co Sponsored By (Career Partner)',
+		link : ''
+	},
+
+	{
+		id : 3,
+		name : 'Colors',
+		image : assoc1,
+		color : '',
+		type: 'In Association with',
+		link : ''
+	},
+
+	{
+		id : 3,
+		name : 'vh1',
+		image : assoc2,
+		color : '',
+		type: 'In Association with',
+		link : ''
+	},
+
+	{
+		id : 3,
+		name : 'Comio Mobiles',
+		image : assoc4,
+		color : '',
+		type: 'In Association with',
 		link : ''
 	},
 	{
 		link : '',
-		id : 3,
+		id : 4,
+		name : 'Hawkeyed',
+		image : photopartner1,
+		color : '',
+		type : 'Official Photography Partner'
+	},
+	{
+		link : '',
+		id : 5,
 		name : 'ZAP',
 		image : technology_partners1,
 		color : '',
@@ -59,123 +112,27 @@ export default [
 	},
 		{
 			link : '',
-		id : 4,
+		id : 6,
 		name : 'JMK',
 		image : overseas,
 		color : '',
-		type: 'Official Overseas Partner'
+		type: 'Official Overseas Establishing Partner'
 	},
 	{
 		link : '',
-		id : 5,
-		name : 'Hero',
+		id : 7,
+		name : 'Hero Cylces',
 		image : cycling_partners,
 		color : '',
-		type: 'Cycling Partners'
+		type: 'Official Cycling Partner'
 	},
 	{
 		link : '',
-		id : 6,
-		name : 'Verka',
-		image : diary_partner1,
-		color : '',
-		type: 'Dairy Partner'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'colors',
-		image : assoc1,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'vh1',
-		image : assoc2,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Guru Nanak Sweets',
-		image : assoc3,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Comio Mobiles',
-		image : assoc4,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Coding Ninjas',
-		image : assoc5,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Mercedes',
-		image : assoc8,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Pokermet',
-		image : assoc9,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Prachi',
-		image : assoc10,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'Strands',
-		image : assoc11,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'GBP',
-		image : assoc6,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : '',
-		id : 7,
-		name : 'GSP',
-		image : assoc7,
-		color : '',
-		type: 'Associate Sponsors'
-	},
-	{
-		link : 'http://www.google.com',
 		id : 8,
-		name : 'TSN',
-		image : offmedia1,
+		name : 'Monte Carlo',
+		image : merch1,
 		color : '',
-		type: 'Official Media Partner'
+		type : 'Merchandise Partner'
 	},
 	{
 		link : '',
@@ -183,22 +140,238 @@ export default [
 		name : '92.7 Big FM',
 		image : fmpartner1,
 		color : '',
-		type: 'FM Partner'
+		type: 'Official Radio Partner'
 	},
 	{
 		link : '',
 		id : 10,
+		name : 'Chandigarh X',
+		image : mag_partner1,
+		color : '',
+		type: 'Official Online Magazine Partner'
+	},
+	{
+		link : '',
+		id : 11,
+		name : 'Verka',
+		image : diary_partner1,
+		color : '',
+		type: 'Dairy Partner'
+	},
+	{
+		link : '',
+		id : 12,
+		name : 'TSN',
+		image : offmedia1,
+		color : '',
+		type: 'Official Inspiring Partner'
+	},
+	{
+		link : '',
+		id : 13,
 		name : 'PU Pulse',
 		image : youthpartner1,
 		color : '',
-		type : 'Youth Partner'
+		type : 'Youth Media Partner'
 	},
 	{
-		link : 'https://www.facebook.com/hawkeyedphotographers/?fref=ts',
-		id : 11,
-		name : 'Hawkeyed',
-		image : photopartner1,
+		link : '',
+		id : 14,
+		name : 'Coding Ninjas',
+		image : coding_prtnr1,
 		color : '',
-		type : 'Official Photography Partner'
+		type: 'Coding Partner'
+	},
+	{
+		link : '',
+		id : 15,
+		name : 'Manforce',
+		image : safety_partner1,
+		color : '',
+		type : 'Safety Partner'
+	},
+	{
+		link : '',
+		id : 16,
+		name : 'Pokermet',
+		image : assoc9,
+		color : '',
+		type: 'Official Poker Partner'
+	},
+	{
+		link : '',
+		id : 17,
+		name : 'Kandhari Beverages',
+		image : food1,
+		color : '',
+		type : 'Kandhari Beverages - Official Coca Cola Partner'
+	},
+	{
+		link : '',
+		id : 18,
+		name : 'Ralco Tyres',
+		image : tyre_partner1,
+		color : '',
+		type : 'Official Tyre Partner'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Mercedes',
+		image : assoc8,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Strands',
+		image : assoc11,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Bajaj Electricals',
+		image : assoc5,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Bestech',
+		image : assoc3,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'GBP',
+		image : assoc6,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'GSP',
+		image : assoc7,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'SBI',
+		image : assoc10,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'I m in',
+		image : assoc12,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'MTV Beats',
+		image : assoc14,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'MTV',
+		image : assoc13,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Droom.in',
+		image : assoc16,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 19,
+		name : 'Comedy Central',
+		image : assoc15,
+		color : '',
+		type: 'Associate Sponsors'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'The CS Kafé',
+		image : food2,
+		color : '',
+		type : 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Naman Passion Et',
+		image : food4,
+		color : '',
+		type: 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Guru Nanak Sweets',
+		image : food5,
+		color : '',
+		type: 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Kamal Caterers',
+		image : food7,
+		color : '',
+		type: 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Wai Wai City',
+		image : food8,
+		color : '',
+		type: 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Major Changs',
+		image : food6,
+		color : '',
+		type: 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Taste Battles',
+		image : food9,
+		color : '',
+		type : 'Food Partners'
+	},
+	{
+		link : '',
+		id : 20,
+		name : 'Eatstreet',
+		image : food3,
+		color : '',
+		type : 'Food Partners'
 	}
 ]
